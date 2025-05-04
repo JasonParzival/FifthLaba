ca464839e22d0587c1d921141d160502529d19ff
JasonParzival <ruslanmuhamed123@gmail.com>
2025-05-04 13:44:59 +0800

    Почти готово

--- FifthLaba/Form1.cs
+++ FifthLaba/Form1.cs
@@ -4,11 +4,24 @@ namespace FifthLaba
 {
     public partial class Form1 : Form
     {
-        MyRectangle myRect; // создадим поле под наш пр€моугольник
+        //MyRectangle myRect; // создадим поле под наш пр€моугольник
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
+
         public Form1()
         {
             InitializeComponent();
-            myRect = new MyRectangle(100, 100, 45); // создать экземпл€р класса
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+
+
+            objects.Add(marker);
+            objects.Add(player);
+            //myRect = new MyRectangle(100, 100, 45); // создать экземпл€р класса
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
@@ -17,9 +30,49 @@ namespace FifthLaba
 
             g.Clear(Color.White);
 
-            g.Transform = myRect.GetTransform(); // устанавливаем новую матрицу
+            foreach (var obj in objects)
+            {
+                // провер€ю было ли пересечение с игроком
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    // и если было вывожу информацию на форму
+                    txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] »грок пересекс€ с {obj}\n" + txtLog.Text;
+                }
 
-            myRect.Render(g); // рендерим как обычно
+                g.Transform = obj.GetTransform();
+
+                obj.Render(g);
+            }
+
+            //g.Transform = myRect.GetTransform(); // устанавливаем новую матрицу
+
+            //myRect.Render(g); // рендерим как обычно
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // расчитываем вектор между игроком и маркером
+            float dx = marker.X - player.X;
+            float dy = marker.Y - player.Y;
+
+            // находим его длину
+            float length = MathF.Sqrt(dx * dx + dy * dy);
+            dx /= length; // нормализуем координаты
+            dy /= length;
+
+            // пересчитываем координаты игрока
+            player.X += dx * 2;
+            player.Y += dy * 2;
+
+            // запрашиваем обновление pbMain
+            // это вызовет метод pbMain_Paint по новой
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            marker.X = e.X;
+            marker.Y = e.Y;
         }
     }
 }
---
+++ FifthLaba/Objects/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthLaba.Objects
+{
+    internal class Player : BaseObject
+    {
+        public Player(float x, float y, float angle) : base(x, y, angle) { 
+        
+        }
+
+        public override void Render(Graphics g)
+        {
+            // и запихиваем туда код из формы
+            g.FillEllipse(
+                new SolidBrush(Color.DeepSkyBlue), 
+                -15, -15, 
+                30, 30
+            );
+            g.DrawEllipse(
+                new Pen(Color.Black, 2),
+                -15, -15, 
+                30, 30
+            );
+            g.DrawLine(new Pen(Color.Black, 2), 0, 0, 25, 0);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-15, -15, 30, 30);
+            return path;
+        }
+    }
+}
